15d5cb10e39fb91ec902821b70e1a3f3c3fe27d6
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 15:54:08 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,5 +6,5 @@ require 'nokogiri'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  projects = kickstarter.css("li.project.grid_4")
+  project = kickstarter.css("li.project.grid_4")
 end
