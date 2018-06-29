103159a56e46dc402db397f831f8e337d3ce042d
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 15:50:08 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,10 +1,10 @@
 # require libraries/modules here
-require_relative './fixtures/kickstarter.html'
+require_relative 'fixtures/kickstarter.html'
 require 'nokogiri'
 
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  
+
 end
